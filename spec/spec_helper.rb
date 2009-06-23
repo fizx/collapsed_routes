5d3550be53f1b2dbe787dc1725f5e8a9d7ca44c5
Kyle Maxwell <kyle@kylemaxwell.com>
2009-06-23 11:22:14 -0700

    wip

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'spec'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'collapsed_routes'
+ENV["RAILS_ENV"] = "test"
+require File.dirname(__FILE__) + "/app/config/environment"
 
 Spec::Runner.configure do |config|
   
