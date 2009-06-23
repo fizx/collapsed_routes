d91063dbe2090ad0903cfc6331da1dbed5e6d3ae
Kyle Maxwell <kyle@kylemaxwell.com>
2009-06-23 11:17:56 -0700

    fake app

---
+++ spec/app/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_app_session',
+  :secret      => '3dae2a90673666107162b6b410fd610434ac7335fc51db39bfaba82495e73aac088e7f682ca1ab23cc04a429a43833cb627a3a6199cda5ca796878713be31c9b'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
